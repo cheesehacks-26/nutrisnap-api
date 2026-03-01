7ddc6e48e70ac132870c5e04f4731d7d80e83b13
divij404 <06.divij.agarwal@gmail.com>
2026-03-01 09:25:30 -0600

    fix: configure CORS to allow Cloudflare Workers origin

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const streakRoutes = require('./src/routes/streak');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://nutrisnap.06-divij-agarwal.workers.dev', 'http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json({ limit: '15mb' }));
 
 app.use('/api/menu', menuRoutes);
